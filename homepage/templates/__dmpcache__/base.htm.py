a841555d55a11979fb9c9b284943ef9d2432823d
isaacmcdgl <isaacmcdgl@gmail.com>
2020-04-15 14:05:38 -0600

    about content

--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1586924670.3852434
+_modified_time = 1586976376.9980028
 _enable_loop = True
 _template_filename = 'C:/Users/isaac/rifftideDMP/homepage/templates/base.htm'
 _template_uri = 'base.htm'
@@ -19,17 +19,17 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        self = context.get('self', UNDEFINED)
         def page_title():
             return render_page_title(context._locals(__M_locals))
-        def page_scripts():
-            return render_page_scripts(context._locals(__M_locals))
-        def page_content():
-            return render_page_content(context._locals(__M_locals))
         settings = context.get('settings', UNDEFINED)
         def page_links():
             return render_page_links(context._locals(__M_locals))
+        def page_content():
+            return render_page_content(context._locals(__M_locals))
+        self = context.get('self', UNDEFINED)
         STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def page_scripts():
+            return render_page_scripts(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('<!DOCTYPE html>\r\n<html>\r\n<meta charset="UTF-8">\r\n    <head>\r\n        <meta charset="utf-8">\r\n        <meta name="author" content="Isaac McDougal">\r\n        <meta name="viewport" content="width=device-width,initial-scale=1.0">\r\n\r\n\r\n\r\n        <title>Rifftide')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'page_title'):
@@ -49,7 +49,7 @@ def render_body(context,**pageargs):
         __M_writer('\r\n\r\n\r\n')
         __M_writer('        <script src="/django_mako_plus/dmp-common.min.js"></script>\r\n        \r\n        <link rel="icon" href="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)(settings.IMAGE_URL))
-        __M_writer('logos/favicon.ico">\r\n\r\n    </head>\r\n    <body>\r\n        <div id="header">\r\n            <div id="nav">\r\n                <div class="navHead">\r\n                    <a><img class="navLogo" src="')
+        __M_writer('logos/favicon.ico">\r\n\r\n    </head>\r\n    <body>\r\n        <div id="header">\r\n            <div id="nav">\r\n                <div class="navHead">\r\n                    <a href="/"><img class="navLogo" src="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)(settings.IMAGE_URL))
         __M_writer('logos/thick logo - darker.png" alt="Rifftide Logo"></a>\r\n                </div>\r\n                <ul>\r\n                    <li>\r\n                        <a href="/about/">About</a>\r\n                    </li>\r\n                    <li>\r\n                        <a href="/cards/">Cards</a>\r\n                    </li>\r\n                </ul>\r\n            </div>\r\n        </div>\r\n        <div id="content" class="content-wrap">\r\n            ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'page_content'):
