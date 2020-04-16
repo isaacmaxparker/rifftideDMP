0f30c72ce0a5b2fdd210add60d101a39733fd99b
isaacmcdgl <isaacmcdgl@gmail.com>
2020-04-16 00:20:34 -0600

    seting false

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import shutil
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOCAL = True
+LOCAL = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
